d09f97333ec739b5552ce0415c901d96334d1863
sujal0018 <sujalkumbhar018@gmail.com>
2025-11-15 19:24:37 +0530

    changes

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 let h1 = document.querySelector('h1');
 let a = 100;
+let timer;
+
 document.addEventListener("DOMContentLoaded", function () {
-    let timer=setInterval(() => {
+    startCounter(1000); 
+});
+
+function startCounter(speed) {
+    timer = setInterval(function () {
+
         a--;
         h1.innerHTML = a;
-        if (a === 0) {
-            h1.innerHTML = "Hacked";
+
+   
+        if (a === 90) {
             clearInterval(timer);
+            startCounter(500); 
         }
-    }, 100);
-})
+
+        if (a === 80) {
+            clearInterval(timer);
+            startCounter(100); 
+        }
+
+        if (a === 0) {
+            h1.innerHTML = "hacked";
+            clearInterval(timer);
+
+        }
+
+    }, speed);
+}
