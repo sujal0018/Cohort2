052177e7aad90362b56d7d919478821a5e52802e
sujal0018 <sujalkumbhar018@gmail.com>
2025-11-15 19:19:46 +0530

    dom task counter

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let h1 = document.querySelector('h1');
-let inc = document.querySelector('.inc');
-let dec = document.querySelector('.dec');
-let a = 0;
-inc.addEventListener('click', function () {
-    a++;
-    h1.innerHTML = a;
-})
-dec.addEventListener('click', function () {
-    a--;
-    h1.innerHTML = a;
+let a = 100;
+document.addEventListener("DOMContentLoaded", function () {
+    let timer=setInterval(() => {
+        a--;
+        h1.innerHTML = a;
+        if (a === 0) {
+            h1.innerHTML = "Hacked";
+            clearInterval(timer);
+        }
+    }, 100);
 })
