923f1225e4181848d07cf1c2af05ef7fee66dbc7
sujal0018 <sujalkumbhar018@gmail.com>
2025-11-16 09:54:52 +0530

    instagram like feature

--- script.js
+++ script.js
@@ -1,33 +1,8 @@
-let h1 = document.querySelector('h1');
-let a = 100;
-let timer;
-
-document.addEventListener("DOMContentLoaded", function () {
-    startCounter(1000); 
-});
-
-function startCounter(speed) {
-    timer = setInterval(function () {
-
-        a--;
-        h1.innerHTML = a;
-
-   
-        if (a === 90) {
-            clearInterval(timer);
-            startCounter(500); 
-        }
-
-        if (a === 80) {
-            clearInterval(timer);
-            startCounter(100); 
-        }
-
-        if (a === 0) {
-            h1.innerHTML = "hacked";
-            clearInterval(timer);
-
-        }
-
-    }, speed);
-}
+let con = document.querySelector(".container");
+let icon = document.querySelector("i");
+con.addEventListener("dblclick", function () {
+    icon.style.transform = "translate(-50%,-50%) scale(1)";
+    setTimeout(() => {
+        icon.style.transform = "translate(-50%,-50%) scale(0)";
+    }, 800);
+})
