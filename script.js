088f296b04cb1ba1614636266b5b481f24ed8ff6
sujal0018 <sujalkumbhar018@gmail.com>
2025-11-17 20:27:59 +0530

    cursor effect dom

--- script.js
+++ script.js
@@ -1,25 +1,6 @@
-let arr = [
-  { name: "Chennai Super Kings",      primary: "yellow",     secondary: "blue"    },
-  { name: "Delhi Capitals",           primary: "blue",       secondary: "red"     },
-  { name: "Gujarat Titans",           primary: "navy",       secondary: "gold"    },
-  { name: "Kolkata Knight Riders",    primary: "purple",     secondary: "gold"    },
-  { name: "Lucknow Super Giants",     primary: "blue",       secondary: "orange"  },
-  { name: "Mumbai Indians",           primary: "deep blue",  secondary: "gold"    },
-  { name: "Punjab Kings",             primary: "crimson",    secondary: "silver"  },
-  { name: "Rajasthan Royals",         primary: "pink",       secondary: "blue"    },
-  { name: "Royal Challengers Bengaluru", primary: "red",     secondary: "black"   },
-  { name: "Sunrisers Hyderabad",      primary: "orange",     secondary: "black"   }
-];
-
-
- 
-let btn=document.querySelector('button');
-let h1 = document.querySelector('h1');
 let main = document.querySelector('main');
-
-btn.addEventListener('click', function () {
-    let a = Math.floor(Math.random() * arr.length);
-    h1.innerHTML = arr[a].name;
-    h1.style.color = arr[a].primary;
-    main.style.backgroundColor = arr[a].secondary;
+let cursor = document.querySelector('.cursor');
+main.addEventListener('mousemove', function (dets) {
+    cursor.style.left = dets.x + "px";
+    cursor.style.top = dets.y + "px";
 })
