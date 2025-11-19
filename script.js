044378e30d086003f3d142fa8337788732608b8e
sujal0018 <sujalkumbhar018@gmail.com>
2025-11-19 10:32:49 +0530

    DOM random page generator project

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-let main = document.querySelector('main');
-let cursor = document.querySelector('.cursor');
-main.addEventListener('mousemove', function (dets) {
-    cursor.style.left = dets.x + "px";
-    cursor.style.top = dets.y + "px";
+let main = document.querySelector("main");
+let button = document.querySelector("button");
+button.addEventListener('click', function () {
+    let div = document.createElement('div');
+    let x = Math.random() * 100;
+    let y = Math.random() * 100;
+    let c1 =Math.floor( Math.random() * 256);
+    let c2 =Math.floor( Math.random() * 256);
+    let c3 =Math.floor( Math.random() * 256);
+    div.style.height = '80px'
+    div.style.width='80px'
+    div.style.backgroundColor = `rgb(${c1},${c2},${c3})`
+    div.style.position = "absolute"
+    div.style.left=x+'%'
+    div.style.top=y+'%'
+    main.appendChild(div)
+    button.style.position='absolute'
+    button.style.zIndex='9'
 })
