ebb6de1c1e0d8da240cd0c3d9e246fbafc7ed831
sujal0018 <sujalkumbhar018@gmail.com>
2025-11-19 17:51:27 +0530

    downloading effect

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-let main = document.querySelector("main");
-let btn = document.querySelector('button');
-
-btn.addEventListener("click", function () {
-    main.classList.toggle("dark");
-
-    if (main.classList.contains("dark")) {
-        btn.textContent = "Light Mode";
-    } else {
-        btn.textContent = "Dark Mode";
-    }
+let button = document.querySelector("button");
+let inner = document.querySelector(".inner");
+let h2 = document.querySelector("h2");
+let grow = 0;
+let card = document.querySelector(".card");
+button.addEventListener("click", function () {
+    button.style.pointerEvents = "none";
+    button.innerHTML = "downloading";
+	let a = setInterval(() => {
+		grow++;
+		h2.innerHTML = grow + "%";
+		inner.style.width = grow + "%";
+	}, 50);
+	setTimeout(() => {
+        clearInterval(a);
+        button.innerHTML = "downloaded";
+        button.style.opacity = 0.5;
+		let h3 = document.createElement("h3");
+		h3.innerHTML = "Download complete";
+		card.appendChild(h3);
+	}, 5000);
 });
